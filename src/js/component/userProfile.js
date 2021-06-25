e6a1e9e4587c6cbbc8979f6e262533bee56ac522
minijuan124 <minijuan124@gmail.com>
2021-06-25 14:23:18 -0400

    usuario

--- src/js/component/userProfile.js
+++ src/js/component/userProfile.js
@@ -36,7 +36,6 @@ export const UserProfile = () => {
 										<h4>
 											{user.name} {user.last_name}
 										</h4>
-										<p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
 									</div>
 								</div>
 							</div>
@@ -107,33 +106,12 @@ export const UserProfile = () => {
 												strokeWidth="2"
 												strokeLinecap="round"
 												strokeLinejoin="round"
-												className="feather feather-globe mr-2 icon-inline">
-												<circle cx="12" cy="12" r="10" />
-												<line x1="2" y1="12" x2="22" y2="12" />
-												<path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
-											</svg>
-											Website
-										</h6>
-										<span className="text-secondary">https://</span>
-									</li>
-									<li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-										<h6 className="mb-0">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												width="24"
-												height="24"
-												viewBox="0 0 24 24"
-												fill="none"
-												stroke="currentColor"
-												strokeWidth="2"
-												strokeLinecap="round"
-												strokeLinejoin="round"
 												className="feather feather-github mr-2 icon-inline">
 												<path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
 											</svg>
 											Github
 										</h6>
-										<span className="text-secondary">minijuan</span>
+										<span className="text-secondary" />
 									</li>
 									<li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
 										<h6 className="mb-0">
@@ -152,7 +130,7 @@ export const UserProfile = () => {
 											</svg>
 											Twitter
 										</h6>
-										<span className="text-secondary">@minijuan</span>
+										<span className="text-secondary">@</span>
 									</li>
 									<li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
 										<h6 className="mb-0">
@@ -173,7 +151,7 @@ export const UserProfile = () => {
 											</svg>
 											Instagram
 										</h6>
-										<span className="text-secondary">minijuan</span>
+										<span className="text-secondary" />
 									</li>
 									<li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
 										<h6 className="mb-0">
@@ -192,7 +170,9 @@ export const UserProfile = () => {
 											</svg>
 											Facebook
 										</h6>
-										<span className="text-secondary">bootdey</span>
+										<span className="text-secondary">
+											{user.name} {user.last_name}
+										</span>
 									</li>
 								</ul>
 							</div>
